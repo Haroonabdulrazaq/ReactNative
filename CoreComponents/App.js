b5ee6edf4547a7446893d770775929f6a232695b
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2023-12-13 12:59:55 +0100

    Add scrollabe component

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,4 +1,4 @@
-import { View, Image, Text, ImageBackground } from "react-native";
+import { View, Image, Text, ScrollView } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
@@ -7,17 +7,32 @@ export default function App() {
       style={{
         flex: 1,
         backgroundColor: "plum",
-        padding: 45,
+        padding: 40,
+        paddingRight: 0,
       }}
     >
-      {/* <Image source={logoImg} style={{ height: 300, width: 300 }} />
-      <Image
-        source={{ uri: "https://picsum.photos/300" }}
-        style={{ height: 300, width: 300 }}
-      /> */}
-      <ImageBackground source={logoImg} style={{ flex: 1 }}>
-        <Text>Image Text</Text>
-      </ImageBackground>
+      <ScrollView style={{ flex: 1 }}>
+        <Image source={logoImg} style={{ height: 300, width: 300 }} />
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+          laudantium modi reiciendis blanditiis. Beatae non, eveniet iste fuga
+          alias ex hic suscipit natus eos odio vel voluptas vitae. Dignissimos,
+          alias. Lorem ipsum dolor, sit amet consectetur adipisicing elit. A
+          unde qui, cumque quisquam veniam porro error natus voluptatum labore
+          doloremque rem laudantium accusamus. Nemo perferendis itaque
+          recusandae voluptatem facilis unde!
+        </Text>
+        <Image source={logoImg} style={{ height: 300, width: 300 }} />
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+          laudantium modi reiciendis blanditiis. Beatae non, eveniet iste fuga
+          alias ex hic suscipit natus eos odio vel voluptas vitae. Dignissimos,
+          alias. Lorem ipsum dolor, sit amet consectetur adipisicing elit. A
+          unde qui, cumque quisquam veniam porro error natus voluptatum labore
+          doloremque rem laudantium accusamus. Nemo perferendis itaque
+          recusandae voluptatem facilis unde!
+        </Text>
+      </ScrollView>
     </View>
   );
 }
