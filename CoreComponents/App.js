d829d3b62a510452db5bc6d34d4fc6079d0e2a14
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2023-12-14 15:02:35 +0100

    Add CustomComponent to CoreComponents

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,32 +1,12 @@
-import { Button, StyleSheet, View, Alert } from "react-native";
+import { StyleSheet, View, StatusBar } from "react-native";
+import Greet from "./component/Greet";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button title="Alert 1" onPress={() => Alert.alert("Invalid date")} />
-      <Button
-        title="Alert 2"
-        onPress={() => Alert.alert("Invalid date", "DOB is invalid")}
-      />
-      <Button
-        title="Alert 3"
-        onPress={() =>
-          Alert.alert("Options", "Click on your preffered option", [
-            {
-              text: "Detail",
-              onPress: () => console.log("Detail Pressed"),
-            },
-            {
-              text: "Cancel",
-              onPress: () => console.log("Cancel Pressed"),
-            },
-            {
-              text: "OK",
-              onPress: () => console.log("OK Pressed"),
-            },
-          ])
-        }
-      />
+      <StatusBar backgroundColor="plum" />
+      <Greet name="Clark Kent" />
+      <Greet name="Bruce Wayne" />
     </View>
   );
 }
